dffb5c7d4a5e71b773ce7bbaa4904d5eff253b99
Sufeng <sniu@hawk.iit.edu>
2013-03-07 17:24:32 -0600

    add FPGA code, need to build IEEE_PROPOSED library first

---
+++ FPGA/real/BoundaryCell/BoundaryCell.vhd
@@ -0,0 +1,181 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:08:49 10/30/2012 
+-- Design Name: 
+-- Module Name:    BoundaryCell - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+--hardware implementation
+library IEEE_PROPOSED;
+use IEEE_PROPOSED.fixed_float_types.all;
+use IEEE_PROPOSED.fixed_pkg.all;
+use IEEE_PROPOSED.float_pkg.all;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+library work;
+use work.QRD_RLS_pkg.all;
+use work.fixed_real_core;
+
+entity BoundaryCell is
+	Generic ( 
+			platform				: string := "FPGA";			-- FPGA/ASIC
+			data_type			: string := "real";			-- real/complex 
+			data_form			: string := "fixed";			-- fixed/floating
+			mode					: string := "streaming";	-- streaming/burst: streaming data need more area and power.
+																		-- consumption
+			oriented				: string := "throughput";	-- throughput/latency, throughput and latency are tradeoff,
+																		-- throughput oriented will provide high speed clock rate
+																		-- latency oriented will provide low circuit latency
+			arch					: string := "unfolded"		-- folded/unfolded: folded can be used for larger matrix size.
+			 );
+	Port ( 
+			clk 					: in	STD_LOGIC;
+			rst 					: in	STD_LOGIC;
+			ce 					: in	STD_LOGIC;
+			 
+			data_in_rdy			: out	STD_LOGIC;
+			data_in				: in	work.QRD_RLS_pkg.DATA_TYPE;
+			data_in_valid		: in	STD_LOGIC;
+			
+			cos_valid 			: out	STD_LOGIC;
+			cos					: out	sfixed (WIDTH_COS-1 downto 0);
+			sin_valid 			: out	STD_LOGIC;
+			sin 					: out	sfixed (WIDTH_SIN-1 downto 0);
+			data_out_valid		: out	STD_LOGIC;
+			data_out				: out	work.QRD_RLS_pkg.DATA_TYPE);
+end BoundaryCell;
+
+architecture Behavioral of BoundaryCell is
+
+begin
+
+FIXED_REAL_GEN: if (data_form = "fixed") and (data_type = "real") generate
+begin	
+	data_out.im_fixed <= (others => '0');
+	data_out.re_float <= (others => '0');
+	data_out.im_float <= (others => '0');
+	FIXED_REAL_INSC: entity work.fixed_real_core
+	generic map (	
+			mode					=> mode,
+			oriented				=> oriented )
+	port map (	
+			clk					=> clk,
+			rst					=> rst,
+			ce						=>	ce,
+			 
+			data_in_rdy			=> data_in_rdy,
+			data_in				=> data_in.re_fixed,
+			data_in_valid		=> data_in_valid,
+		 
+			cos_valid			=> cos_valid,
+			cos					=> cos,
+			sin_valid			=> sin_valid,
+			sin					=> sin,
+			data_out_valid		=> data_out_valid,
+			data_out				=> data_out.re_fixed );
+end generate FIXED_REAL_GEN;
+
+FLOAT_REAL_GEN: if (data_form = "float") and (data_type = "real") generate
+begin
+	data_out.re_fixed <= (others => '0');
+	data_out.im_fixed <= (others => '0');
+	data_out.im_float <= (others => '0');
+	FLOAT_REAL_INSC: entity work.float_real_core
+	generic map (	
+			mode					=> mode,
+			oriented				=> oriented )
+	port map (	
+			clk					=> clk,
+			rst					=> rst,
+			ce						=>	ce,
+		 
+			data_in_rdy			=> data_in_rdy,
+			data_in				=> data_in.re_float,
+			data_in_valid		=> data_in_valid,
+		 
+			cos_valid			=> cos_valid,
+			cos					=> cos,
+			sin_valid			=> sin_valid,
+			sin					=> sin,
+			data_out_valid		=> data_out_valid,
+			data_out				=> data_out.re_float );
+end generate FLOAT_REAL_GEN;
+
+FIXED_COMP_GEN: if (data_form = "fixed") and (data_type = "complex") generate
+begin
+	data_out.re_float <= (others => '0');
+	data_out.im_float <= (others => '0');
+	FIXED_COMP_INSC: entity work.fixed_comp_core
+	generic map (	
+			mode					=> mode,
+			oriented				=> oriented )
+	port map (	
+			clk					=> clk,
+			rst					=> rst,
+			ce						=>	ce,
+		 
+			data_in_rdy			=> data_in_rdy,
+			data_in_re			=> data_in.re_fixed,
+			data_in_im			=> data_in.im_fixed,
+			data_in_valid		=> data_in_valid,
+		 
+			cos_valid			=> cos_valid,
+			cos					=> cos,
+			sin_valid			=> sin_valid,
+			sin					=> sin,
+			data_out_valid		=> data_out_valid,
+			data_out_re			=> data_out.re_fixed,
+			data_out_im			=> data_out.im_fixed );
+end generate FIXED_COMP_GEN;
+
+FLOAT_COMP_GEN: if (data_form = "float") and (data_type = "complex") generate
+begin
+	data_out.re_fixed <= (others => '0');
+	data_out.im_fixed <= (others => '0');
+	FLOAT_COMP_INSC: entity work.float_comp_core
+	generic map (	
+			mode					=> mode,
+			oriented				=> oriented )
+	port map (	
+			clk					=> clk,
+			rst					=> rst,
+			ce						=>	ce,
+			 
+			data_in_rdy			=> data_in_rdy,
+			data_in_re			=> data_in.re_float,
+			data_in_im			=> data_in.im_float,
+			data_in_valid		=> data_in_valid,
+		 
+			cos_valid			=> cos_valid,
+			cos					=> cos,
+			sin_valid			=> sin_valid,
+			sin					=> sin,
+			data_out_valid		=> data_out_valid,
+			data_out_re			=> data_out.re_float,
+			data_out_im			=> data_out.im_float );
+end generate FLOAT_COMP_GEN;
+
+end Behavioral;
+
---
+++ FPGA/real/BoundaryCell/QRD_RLS_pkg.vhd
@@ -0,0 +1,141 @@
+--
+--	Package File Template
+--
+--	Purpose: This package defines supplemental types, subtypes, 
+--		 constants, and functions 
+--
+--   To use any of the example code shown below, uncomment the lines and modify as necessary
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.math_real.all;
+
+library IEEE_PROPOSED;
+use IEEE_PROPOSED.fixed_float_types.all;
+use IEEE_PROPOSED.fixed_pkg.all;
+use IEEE_PROPOSED.float_pkg.all;
+
+package QRD_RLS_pkg is
+
+--------------------------------------------------------------------------------------
+--								Boundary cell generic 
+--------------------------------------------------------------------------------------
+	-- fixed point
+	constant DATA_INT					: integer := 2;				-- input width integer part
+	constant DATA_FRA					: integer := 13;				-- input width fractional part
+	constant WIDTH_DATA_FIXED				: integer := DATA_INT + DATA_FRA + 1;
+		
+	constant COS_INT					: integer := 1;				-- cosine width integer part
+	constant COS_FRA					: integer := 14;				-- cosine width fractional part
+	constant WIDTH_COS_FIXED		: integer := COS_INT + COS_FRA + 1;
+	constant COS_EXP					: integer := 8;				-- cosine
+	constant COS_FPF					: integer := 23;				-- cosine
+	constant	WIDTH_COS_FLOAT		: integer := COS_EXP + COS_FPF + 1;
+	
+	constant SIN_INT					: integer := 1;				-- sine width integer part
+	constant SIN_FRA					: integer := 14;				-- sine width fractional part
+	constant WIDTH_SIN_FIXED		: integer := SIN_INT + SIN_FRA + 1;
+	constant SIN_EXP					: integer := 8;				-- cosine
+	constant SIN_FPF					: integer := 23;				-- cosine
+	constant WIDTH_SIN_FLOAT		: integer := SIN_EXP + SIN_FPF + 1;
+	
+	-- floating point
+	constant DATA_EXP					: integer := 8;				-- floating point exp
+	constant DATA_FPF					: integer := 23;				-- floating point fractinoal (FPF)
+	constant WIDTH_DATA_FLOAT		: integer := DATA_FPF + DATA_EXP + 1;
+	
+	constant lamda						: real := 0.98;
+	constant LAMDA_INT				: integer := 1;				-- lamda integer part width
+	constant LAMDA_FRA				: integer := 14;				-- lamda fractional part width
+	constant LAMDA_EXP				: integer := 8;
+	constant LAMDA_FPF				: integer := 23;
+	
+	-- testbench generic
+   constant VECTOR_LENGTH			: integer := 100;
+	type din_t_fixed is array (VECTOR_LENGTH-1 downto 0) of std_logic_vector(WIDTH_FIXED-1 downto 0);
+	type dout_t_fixed is array (VECTOR_LENGTH-1 downto 0) of std_logic_vector(WIDTH_FIXED-1 downto 0);
+	type din_t_float is array (VECTOR_LENGTH-1 downto 0) of std_logic_vector(WIDTH_FLOAT-1 downto 0);
+	type dout_t_float is array (VECTOR_LENGTH-1 downto 0) of std_logic_vector(WIDTH_FLOAT-1 downto 0);
+
+type DATA_TYPE is	record
+	re_fixed								: sfixed(DATA_INT downto -DATA_FRA);
+	im_fixed								: sfixed(DATA_INT downto -DATA_FRA);
+	re_float								: float(DATA_EXP downto -DATA_FPF);
+	im_float								: float(DATA_EXP downto -DATA_FPF);
+end record;
+
+--
+-- Declare constants
+--
+-- constant <constant_name>		: time := <time_unit> ns;
+-- constant <constant_name>		: integer := <value;
+--
+-- Declare functions and procedure
+--
+-- function <function_name>  (signal <signal_name> : in <type_declaration>) return <type_declaration>;
+-- procedure <procedure_name> (<type_declaration> <constant_name>	: in <type_declaration>);
+	
+end QRD_RLS_pkg;
+
+package body QRD_RLS_pkg is
+
+---- Example 1
+--  function <function_name>  (signal <signal_name> : in <type_declaration>  ) return <type_declaration> is
+--    variable <variable_name>     : <type_declaration>;
+--  begin
+--    <variable_name> := <signal_name> xor <signal_name>;
+--    return <variable_name>; 
+--  end <function_name>;
+
+---- Example 2
+--  function <function_name>  (signal <signal_name> : in <type_declaration>;
+--                         signal <signal_name>   : in <type_declaration>  ) return <type_declaration> is
+--  begin
+--    if (<signal_name> = '1') then
+--      return <signal_name>;
+--    else
+--      return 'Z';
+--    end if;
+--  end <function_name>;
+
+---- Procedure Example
+--  procedure <procedure_name>  (<type_declaration> <constant_name>  : in <type_declaration>) is
+--    
+--  begin
+--    
+--  end <procedure_name>;
+
+impure function lamda_fixed(lamda : real) return sfixed is
+	variable res : sfixed(LAMDA_INT downto -LAMDA_FRA);
+	begin
+		res := to_sfixed(lamda, res);							-- lamda =< 1
+	return res;
+end function;
+
+--impure function lamda_float(lamda : real) return float is
+--	variable res : float;
+--	begin
+--		res <= to_float(lamda, , );
+--	return res;
+--end function;
+
+--function trounding (
+--	data 				: sfixed;
+--	left_index		: integer;
+--	right_index		: integer) 
+--	return sfixed is
+--	
+--	variable res : sfixed(left_index downto right_index);
+--	variable temp : std_logic_vector(data'high + data'low downto 0);
+--	variable lsb : std_logic;
+--begin
+--	temp := to_slv(data);
+--	temp()&(temp(1) or temp(0));
+--	
+--	res := data(left_index downto right_index+1) & lsb;
+--	return res;
+--end function;
+
+end QRD_RLS_pkg;
